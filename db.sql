136bea2ee6ee7cebc458b12645f053c93611341e
Mostafa Hosseini <s.mostafa.2001.1380@outlook.com>
2022-08-19 19:23:39 +0430

    initial-commit

---
+++ db.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE product_tbl
+(
+  id bigint NOT NULL, --
+  name_product character varying,
+  date_manufacture date, --
+  price double precision, --
+  quantity integer, --
+  CONSTRAINT uid PRIMARY KEY (id )
+);
+insert into product_tbl (id, name_product, date_manufacture, price, quantity) values (1, 'Macbook Air', '12/02/2021', 1200.00, 3);
